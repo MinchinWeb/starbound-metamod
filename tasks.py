6fe25c8cec82ed0277ce51346d620f44a930978a
MinchinWeb <w_minchin@hotmail.com>
2021-03-08 09:40:14 -0700

    black-ify

--- tasks.py
+++ tasks.py
@@ -1,24 +1,26 @@
-from pathlib import Path
 import sys
+from pathlib import Path
 
 import colorama
-from invoke import task, run
 import minchin.text as text
 import winshell
+from invoke import run, task
 
-SB_STEAM_ID = '211820'
+SB_STEAM_ID = "211820"
 
-SB_BASE_DIR = Path(r'C:\Program Files (x86)\Steam\steamapps\common\Starbound')
-SB_STEAM_MODS_DIR = Path(r'C:\Program Files (x86)\Steam\steamapps\workshop\content\211820')
-SB_UNIVERSE_DIR = SB_BASE_DIR / 'storage' / 'universe'
-SB_PLAYER_DIR = SB_BASE_DIR / 'storage' / 'player'
+SB_BASE_DIR = Path(r"C:\Program Files (x86)\Steam\steamapps\common\Starbound")
+SB_STEAM_MODS_DIR = Path(
+    r"C:\Program Files (x86)\Steam\steamapps\workshop\content\211820"
+)
+SB_UNIVERSE_DIR = SB_BASE_DIR / "storage" / "universe"
+SB_PLAYER_DIR = SB_BASE_DIR / "storage" / "player"
 
-SB_ASSETS = SB_BASE_DIR / 'assets' / 'packed.pak'
+SB_ASSETS = SB_BASE_DIR / "assets" / "packed.pak"
 
-SB_ASSET_PACKER = SB_BASE_DIR / 'win32' / 'asset_packer.exe'
-SB_ASSET_UNPACKER = SB_BASE_DIR / 'win32' / 'asset_unpacker.exe'
-SB_DUMP_JSON = SB_BASE_DIR / 'win32' / 'dump_versioned_json.exe'
-SB_LOAD_JSON = SB_BASE_DIR / 'win32' / 'make_version_json.exe'
+SB_ASSET_PACKER = SB_BASE_DIR / "win32" / "asset_packer.exe"
+SB_ASSET_UNPACKER = SB_BASE_DIR / "win32" / "asset_unpacker.exe"
+SB_DUMP_JSON = SB_BASE_DIR / "win32" / "dump_versioned_json.exe"
+SB_LOAD_JSON = SB_BASE_DIR / "win32" / "make_version_json.exe"
 
 STEAM_SB_MOD_URL = "http://steamcommunity.com/sharedfiles/filedetails/?id={}"
 
@@ -36,7 +38,7 @@ def hello_world(ctx):
 
 
 @task
-def unpack_assets(ctx, source=SB_ASSETS, destination = 'unpacked'):
+def unpack_assets(ctx, source=SB_ASSETS, destination="unpacked"):
     """Unpack the game assets."""
     text.title("Asset Unpacker")
 
@@ -50,16 +52,16 @@ def unpack_assets(ctx, source=SB_ASSETS, destination = 'unpacked'):
         print("Game Assets: {}MISSING{}".format(RED, RESET_ALL))
         print("Exiting...")
         sys.exit(1)
-    
+
     # check destination folder
     if destination.exists():
         # test if folder is empty
-        if list(destination.rglob('*')):
+        if list(destination.rglob("*")):
             print("Destination Folder: {}Exists, Not Empty{}".format(YELLOW, RESET_ALL))
             print("    {}".format(destination.resolve()))
             ans = text.query_yes_no("    Empty Folder?")
             if ans == text.Answers.YES:
-                winshell.delete_file(destination.rglob('*'), silent=True)
+                winshell.delete_file(destination.rglob("*"), silent=True)
         else:
             print("Destination Folder: {}Exists, Empty{}!".format(GREEN, RESET_ALL))
     else:
@@ -83,9 +85,14 @@ def unpack_assets(ctx, source=SB_ASSETS, destination = 'unpacked'):
 
 
 @task
-def unpack_steam_mods(ctx, source=SB_STEAM_MODS_DIR,
-                      destination = 'unpacked-mods', override_existing=False,
-                      skip_existing=False, verbose=False):
+def unpack_steam_mods(
+    ctx,
+    source=SB_STEAM_MODS_DIR,
+    destination="unpacked-mods",
+    override_existing=False,
+    skip_existing=False,
+    verbose=False,
+):
     """Unpack all steam mods."""
     text.title("Steam Mod Unpacker")
 
@@ -104,16 +111,16 @@ def unpack_steam_mods(ctx, source=SB_STEAM_MODS_DIR,
         print("Steam Mods: {}MISSING{}".format(RED, RESET_ALL))
         print("Exiting...")
         sys.exit(1)
-    
+
     # check destination folder
     if destination.exists():
         # test if folder is empty
-        if list(destination.rglob('*')):
+        if list(destination.rglob("*")):
             print("Destination Folder: {}Exists, Not Empty{}".format(YELLOW, RESET_ALL))
             print("    {}".format(destination.resolve()))
             ans = text.query_yes_no("    Empty Folder?", default="no")
             if ans == text.Answers.YES:
-                winshell.delete_file(destination.rglob('*'), silent=True)
+                winshell.delete_file(destination.rglob("*"), silent=True)
         else:
             print("Destination Folder: {}Exists, Empty{}!".format(GREEN, RESET_ALL))
     else:
@@ -134,10 +141,13 @@ def unpack_steam_mods(ctx, source=SB_STEAM_MODS_DIR,
             override_this = False
             if mod_destination.exists():
                 if not override_existing and not skip_existing:
-                    ans = text.query_yes_no_all_none(("Destination folder for "
-                                                      "mod {} exists. Override?"
-                                                      .format(mod_id)),
-                                                     default='none')
+                    ans = text.query_yes_no_all_none(
+                        (
+                            "Destination folder for "
+                            "mod {} exists. Override?".format(mod_id)
+                        ),
+                        default="none",
+                    )
                     if ans == text.Answers.YES:
                         override_this = True
                     elif ans == text.Answers.NO:
@@ -153,17 +163,17 @@ def unpack_steam_mods(ctx, source=SB_STEAM_MODS_DIR,
                     winshell.rmdir(mod_destination)
                 else:
                     skip_this = True
-                
+
             # the actual unpacking!
             if skip_this:
                 if verbose:
                     print("    {}Skipping{} {}...".format(YELLOW, RESET_ALL, mod_id))
                 count_skipped += 1
             else:
-                if (fn / 'contents.pak').exists():
+                if (fn / "contents.pak").exists():
                     cmd = '"{}" "{}" "{}"'.format(
                         SB_ASSET_UNPACKER,
-                        fn / 'contents.pak',
+                        fn / "contents.pak",
                         mod_destination,
                     )
                     try:
@@ -175,14 +185,20 @@ def unpack_steam_mods(ctx, source=SB_STEAM_MODS_DIR,
                     else:
                         count_unpacked += 1
                 else:
-                    print("    {}Skipping{}, missing file: {}".format(
-                        YELLOW, RESET_ALL, (fn / 'contents.pak')
-                    ))
+                    print(
+                        "    {}Skipping{}, missing file: {}".format(
+                            YELLOW, RESET_ALL, (fn / "contents.pak")
+                        )
+                    )
                     count_skipped += 1
 
     print()
-    print(("{}Done!{} {} mods unpacked, {} skipped, and {} errorred."
-           .format(GREEN, RESET_ALL, count_unpacked, count_skipped,
-                   count_errorred)))
+    print(
+        (
+            "{}Done!{} {} mods unpacked, {} skipped, and {} errorred.".format(
+                GREEN, RESET_ALL, count_unpacked, count_skipped, count_errorred
+            )
+        )
+    )
     if error_list:
         print("{}Errors{}: {}".format(YELLOW, RESET_ALL, ", ".join(error_list)))
