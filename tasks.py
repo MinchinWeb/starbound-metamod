cf98b52700f45b6d43f2714da6917218943e7c3c
MinchinWeb <w_minchin@hotmail.com>
2021-03-08 10:53:59 -0700

    Add task to copy mods to server directory

--- tasks.py
+++ tasks.py
@@ -202,3 +202,152 @@ def unpack_steam_mods(
     )
     if error_list:
         print("{}Errors{}: {}".format(YELLOW, RESET_ALL, ", ".join(error_list)))
+
+
+@task(iterable=["mods"])
+def copy_mods_to_server(
+    ctx,
+    source=SB_STEAM_MODS_DIR,
+    destination=None,
+    override_existing=False,
+    skip_existing=False,
+    update_existing=False,
+    verbose=False,
+    mods=None,
+):
+    """Copy and 'flat file' steam mods to server mod directory."""
+    text.title("Copy and 'flat file' steam mods to server mod directory.")
+
+    count_copied = 0
+    count_updated = 0
+    count_skipped = 0
+    count_errorred = 0
+    error_list = []
+
+    source = Path(source)
+    # check source file
+    if source.exists():
+        print("Steam Mods: {}FOUND{}!".format(GREEN, RESET_ALL))
+    else:
+        print("Steam Mods: {}MISSING{}".format(RED, RESET_ALL))
+        print("Exiting...")
+        sys.exit(1)
+
+    if destination:
+        destination = Path(destination)
+    else:
+        print("Destination: {}UNDEFINED{}".format(RED, RESET_ALL))
+        print("Exiting...")
+        sys.exit(1)
+
+    # check destination folder
+    if destination.exists():
+        # test if folder is empty
+        if list(destination.rglob("*")):
+            print("Destination Folder: {}Exists, Not Empty{}".format(YELLOW, RESET_ALL))
+            print("    {}".format(destination.resolve()))
+            ans = text.query_yes_no("    Empty Folder?", default="no")
+            if ans == text.Answers.YES:
+                winshell.delete_file(destination.rglob("*"), silent=True)
+        else:
+            print("Destination Folder: {}Exists, Empty{}!".format(GREEN, RESET_ALL))
+    else:
+        print("Destination Folder: {}MISSING{}".format(YELLOW, RESET_ALL))
+        ans = text.query_yes_no("    Create?")
+        if ans == text.Answers.YES:
+            destination.mkdir(parents=True)
+        else:
+            print("Exiting...")
+            sys.exit(1)
+
+    if not mods:
+        if (destination / "mods.txt").exists():
+            with (destination / "mods.txt").open() as f:
+                # drop everything after '#'
+                mods = [line.split("#", 1)[0].strip() for line in f]
+                # remove empty lines
+                mods = [line for line in mods if line]
+            print("Using 'mods.txt' in destination folder.")
+        else:
+            print(
+                "{}Provide a list of the steam IDs of the mods you want to copy{}".format(
+                    RED, RESET_ALL
+                )
+            )
+            print("Exiting...")
+            sys.exit(1)
+
+    print("Coping...")
+    for fn in source.iterdir():
+        if fn.is_dir() and (fn.name in mods or mods[0].lower() == "all"):
+            mod_source = fn / "contents.pak"
+            if not mod_source.exists():
+                print(
+                    f"{YELLOW}Mod {mod_id} in non-typical format. Skipping.{RESET_ALL}"
+                )
+                count_skipped += 1
+                continue
+
+            mod_id = fn.name
+            mod_destination = destination / f"{mod_id}.pak"
+            skip_this = False
+            override_this = False
+
+            if mod_destination.exists():
+                if update_existing:
+                    # last modified time
+                    source_mtime = mod_source.stat().st_mtime
+                    destination_mtime = mod_destination.stat().st_mtime
+
+                    if source_mtime > destination_mtime:
+                        override_this = True
+                        count_updated += 1
+
+                if not override_existing and not skip_existing:
+                    ans = text.query_yes_no_all_none(
+                        (
+                            "Destination file for "
+                            "mod {} exists. Overwrite?".format(mod_id)
+                        ),
+                        default="none",
+                    )
+                    if ans == text.Answers.YES:
+                        override_this = True
+                    elif ans == text.Answers.NO:
+                        skip_this = True
+                    elif ans == text.Answers.ALL:
+                        override_existing = True
+                        override_this = True
+                    elif ans == text.Answers.NONE:
+                        skip_existing = True
+                        skip_this = True
+
+                if override_this or override_existing:
+                    winshell.delete_file(str(mod_destination))
+                else:
+                    skip_this = True
+
+            # the actual unpacking!
+            if skip_this:
+                if verbose:
+                    print("    {}Skipping{} {}...".format(YELLOW, RESET_ALL, mod_id))
+                count_skipped += 1
+            else:
+                winshell.copy_file(str(mod_source), str(mod_destination))
+                count_copied += 1
+
+    print()
+    print(
+        (
+            "{}Done!{} {} mods copied, {} updated, {} skipped, and {} errorred.".format(
+                GREEN,
+                RESET_ALL,
+                count_copied,
+                count_updated,
+                count_skipped,
+                count_errorred,
+            )
+        )
+    )
+    if error_list:
+        print("{}Errors{}: {}".format(YELLOW, RESET_ALL, ", ".join(error_list)))
